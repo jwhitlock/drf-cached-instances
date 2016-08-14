d07164de5c2570804be333a4718b9c4e96561cc2
John Whitlock <jwhitlock@mozilla.com>
2016-08-14 14:53:29 -0500

    Remove deprecated django.conf.urls.patterns
    Deprecated in Django 1.8, removed in 1.10:
    https://docs.djangoproject.com/en/1.10/releases/1.8/#django-conf-urls-patterns

--- sample_poll_app/urls.py
+++ sample_poll_app/urls.py
@@ -1,5 +1,5 @@
 """URL routes for the sample app."""
-from django.conf.urls import include, patterns, url
+from django.conf.urls import include, url
 from django.views.generic import TemplateView
 
 from rest_framework.routers import DefaultRouter
@@ -11,11 +11,10 @@ router.register(r'users', UserViewSet)
 router.register(r'questions', QuestionViewSet)
 router.register(r'choices', ChoiceViewSet)
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^$', TemplateView.as_view(
         template_name='sample_poll_app/home.html'), name='home'),
     url(r'^api-auth/', include(
         'rest_framework.urls', namespace='rest_framework')),
     url(r'^api/', include(router.urls))
-)
+]
--- sample_site/urls.py
+++ sample_site/urls.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
 """URL routing patterns for drf-cached-instances."""
 
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 
 from sample_poll_app.urls import urlpatterns as sample_urlpatterns
 
 admin.autodiscover()
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include(sample_urlpatterns)),
-)
+]
